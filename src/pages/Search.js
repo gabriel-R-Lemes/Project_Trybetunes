a5753c01f6f3f11f399eb224820cfe852ab974df
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-10-03 19:22:11 -0300

    Realização do Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,11 +3,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isDisabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.shoudEnable());
+  };
+
+  handleClick = async () => {
+  };
+
+  shoudEnable = () => {
+    const { name } = this.state;
+    const minLength = 2;
+    this.setState({ isDisabled: (name.length < minLength) });
+  };
+
   render() {
+    const { name, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Area Pesquisa</p>
+        <form>
+          <input
+            type="text"
+            placeholder="Digite o nome do Artista"
+            name="name"
+            id="name"
+            defaultValue={ name }
+            onChange={ this.onInputChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
