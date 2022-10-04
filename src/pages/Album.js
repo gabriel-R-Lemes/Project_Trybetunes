6bab6a5b60f18294905e9fe34eae4f4c64b811de
Gabriel Lemes <rlemes.gabriel@gmail.com>
2022-10-04 19:04:11 -0300

    Realização do Requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,20 +1,68 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+      collection: {},
+    };
+  }
+
+  getMusicsFromAPI = async () => {
+    const { match } = this.props;
+    const result = await getMusics(match.params.id);
+    const onlyTracks = result.filter((album, index) => {
+      if (index > 0) {
+        return album;
+      } return null;
+    });
+    this.setState({ collection: result[0] }, this.setState({ results: onlyTracks }));
+  };
+
   render() {
+    const { results, collection } = this.state;
+    this.getMusicsFromAPI();
     return (
       <div data-testid="page-album">
         <Header />
-        <p>Album!!!</p>
+        <h2
+          data-testid="artist-name"
+        >
+          Artist
+          <p>{ collection.artistName }</p>
+        </h2>
+        <h3
+          data-testid="album-name"
+        >
+          <p>{ collection.collectionName }</p>
+          <p>{ collection.artistName }</p>
+        </h3>
+        { results.map((album) => (
+          <div key={ album.trackId }>
+            <br />
+            <MusicCard
+              trackName={ album.trackName }
+              previewUrl={ album.previewUrl }
+              key={ album.trackName }
+            />
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-// Login.propTypes = {
-
-// };
+Album.propTypes = {
+  match: {
+    params: {
+      id: PropTypes.string.isRequired,
+    },
+  }.isRequired,
+};
 
 export default Album;
